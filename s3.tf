13e061e72eb785e3bee3e30ebda6f33f2f8769b1
CamiloBaeza <102620801+CamiloBaeza@users.noreply.github.com>
2022-12-05 21:22:25 +0000

    Automated Report

--- s3.tf
+++ s3.tf
@@ -1,7 +1,15 @@
-resource "aws_s3_object" "s3_object_procesados" {
+resource "aws_s3_object" "s3_object_procesados_SD" {
   for_each = fileset("./Studio-develop/","**")
   bucket = "tarrotomado"
   key = "Studio-develop/${each.value}"
   source = "./Studio-develop/${each.value}"
   etag = filemd5("./Studio-develop/${each.value}")
+}
+
+resource "aws_s3_object" "s3_object_procesados_SQ" {
+  for_each = fileset("./Studio-qa/","**")
+  bucket = "tarrotomado"
+  key = "Studio-qa/${each.value}"
+  source = "./Studio-qa/${each.value}"
+  etag = filemd5("./Studio-qa/${each.value}")
 }
