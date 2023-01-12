148cee0785385a7cb6a69c3b2164f9cdc8283eef
CamiloBaeza <102620801+CamiloBaeza@users.noreply.github.com>
2023-01-12 20:56:13 +0000

    holi

--- code/lambda/lambda_function.py
+++ code/lambda/lambda_function.py
@@ -1,6 +1,14 @@
 import json
 import os
-def lambda_handler(event, context):
+from botocore.exceptions import ClientError
 
-    
-    return os.environ["BUCKET_TARRO"]
+MAX_DELETE_BATCH_SIZE = 100 
+logger = logging.getLogger()
+logger.setLevel(os.getenv("LOG_LEVEL", logging.INFO))
+
+def lambda_handler(event, context):
+    try:
+        return os.environ["BUCKET_TARRO"]
+    except ClientError as error:
+        logger.info("Unexpected error: %s" %error)
+        return
