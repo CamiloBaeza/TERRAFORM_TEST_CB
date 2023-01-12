eb116e13147f2fb1745adf6f3c1b6bf0acd2a00b
CamiloBaeza <102620801+CamiloBaeza@users.noreply.github.com>
2023-01-12 21:01:06 +0000

    weni

--- lambda.tf
+++ lambda.tf
@@ -15,7 +15,7 @@ resource "aws_lambda_function" "lambdita" {
   timeout          = 900
   environment {
     variables = {
-      BUCKET_TARRO = "${aws_s3_bucket.tarrocaido2.id}"
+      BUCKET_TARRO = "tarrocaido2-85326974528sssd8"
     }
   }
 }
