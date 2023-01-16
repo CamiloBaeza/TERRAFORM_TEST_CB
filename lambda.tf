7e2c8837f4a5499ce26e71930e30700c30c258b3
CamiloBaeza <102620801+CamiloBaeza@users.noreply.github.com>
2023-01-16 23:34:40 +0000

    p

--- lambda.tf
+++ lambda.tf
@@ -1,22 +1,22 @@
-data "archive_file" "zip_the_python_code_main_tarrocaido" {
-  type        = "zip"
-  source_dir  = "./code/lambda/"
-  output_path = "./code/lambda/lambda_function.py.zip"
-}
+# data "archive_file" "zip_the_python_code_main_tarrocaido" {
+#   type        = "zip"
+#   source_dir  = "./code/lambda/"
+#   output_path = "./code/lambda/lambda_function.py.zip"
+# }
 
-resource "aws_lambda_function" "lambdita" {
-  filename         = "./code/lambda/lambda_function.py.zip"
-  source_code_hash = data.archive_file.zip_the_python_code_main_tarrocaido.output_base64sha256
-  function_name    = local.lambdanamefunc
-  role             = "arn:aws:iam::042670738437:role/Rol-lambdatarrocaido"
-  description      = "blbla"
-  handler          = "main.handler"
-  runtime          = "python3.8"
-  timeout          = 900
-  environment {
-    variables = {
-      BUCKET_TARRO = aws_s3_bucket.tarrocaido2.id
-    }
-  }
-}
+# resource "aws_lambda_function" "lambdita" {
+#   filename         = "./code/lambda/lambda_function.py.zip"
+#   source_code_hash = data.archive_file.zip_the_python_code_main_tarrocaido.output_base64sha256
+#   function_name    = local.lambdanamefunc
+#   role             = "arn:aws:iam::042670738437:role/Rol-lambdatarrocaido"
+#   description      = "blbla"
+#   handler          = "main.handler"
+#   runtime          = "python3.8"
+#   timeout          = 900
+#   environment {
+#     variables = {
+#       BUCKET_TARRO = aws_s3_bucket.tarrocaido2.id
+#     }
+#   }
+# }
 
